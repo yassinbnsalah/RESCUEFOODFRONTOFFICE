f74f928e20b16ae226e3efaa60e4dff240f238cb
yassinebnsalah <yacinbnsalh@gmail.com>
2024-09-19 22:55:21 +0100

    first commit

---
+++ resources/views/home/hero.blade.php
@@ -0,0 +1,17 @@
+@extends('main')
+@section('hero')
+<section class="bg-darker bg-size-cover bg-position-center py-5 py-lg-10" style="background-image: url(img/food-delivery/hero-bg.jpg);">
+    <div class="container pb-5">
+      <div class="row justify-content-center pb-5">
+        <div class="col-xl-6 col-lg-7 col-md-8 col-sm-10 text-center">
+          <h5 class="text-light fw-light">#1 Rescue Food Project Since 2024</h5>
+          <div class="h1 text-light mb-3 pb-4">We deliver your favorite food fresh &amp; fast in
+            <div class="dropdown d-inline-block ms-1"><a class="dropdown-toggle" href="#" data-bs-toggle="dropdown">New York</a>
+              <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item fs-base" href="#">Los Angeles</a><a class="dropdown-item fs-base" href="#">Chicago</a><a class="dropdown-item fs-base" href="#">Houston</a><a class="dropdown-item fs-base" href="#">Philadelphia</a><a class="dropdown-item fs-base" href="#">San Diego</a><a class="dropdown-item fs-base" href="#">Miami</a></div>
+            </div>
+          </div><a class="btn btn-primary" href="#cuisine" data-scroll>What do you want to eat?</a>
+        </div>
+      </div>
+    </div>
+  </section>
+@endsection
---
+++ resources/views/shared/nav.blade.php
@@ -0,0 +1,140 @@
+<header
+  class="navbar d-block navbar-sticky navbar-expand-lg navbar-light bg-light">
+  <div class="container"><a
+      class="navbar-brand d-none d-sm-block me-4 order-lg-1" href="index.html">
+      <img src="img/logo-dark.png" width="142" alt="Cartzilla"></a><a
+      class="navbar-brand d-sm-none me-2 order-lg-1" href="index.html"><img
+        src="img/logo-icon.png" width="74" alt="Cartzilla"></a>
+    <div class="navbar-toolbar d-flex align-items-center order-lg-3">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"><span
+          class="navbar-toggler-icon"></span></button><a
+        class="navbar-tool d-none d-lg-flex" href="javascript:void(0)"
+        data-bs-toggle="collapse" data-bs-target="#searchBox" role="button"
+        aria-expanded="false" aria-controls="searchBox"><span
+          class="navbar-tool-tooltip">Search</span>
+        <div class="navbar-tool-icon-box"><i
+            class="navbar-tool-icon ci-search"></i></div></a><a
+        class="navbar-tool ms-2" href="#signin-modal"
+        data-bs-toggle="modal"><span class="navbar-tool-tooltip">Account</span>
+        <div class="navbar-tool-icon-box"><i
+            class="navbar-tool-icon ci-user"></i></div></a>
+    </div>
+    <div class="collapse navbar-collapse me-auto order-lg-2"
+      id="navbarCollapse">
+      <!-- Search (mobile)-->
+      <div class="d-lg-none py-3">
+        <div class="input-group"><i
+            class="ci-search position-absolute top-50 start-0 translate-middle-y ms-3"></i>
+          <input class="form-control rounded-start" type="text"
+            placeholder="What do you need?">
+        </div>
+      </div>
+      <!-- Location dropdown-->
+      
+      <!-- Primary menu-->
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown"><a class="nav-link dropdown-toggle"
+            href="#" data-bs-toggle="dropdown">Food Categoryies</a>
+          <div class="dropdown-menu px-2 ps-0 pb-4">
+            <div class="d-flex">
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/burger.svg" width="60"
+                      alt="Burgers &amp; Fries">
+                    <h6 class="fs-base text-center">Burgers &amp;
+                      Fries</h6></a></div>
+              </div>
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/noodles.svg" width="60"
+                      alt="Noodles">
+                    <h6 class="fs-base text-center">Noodles</h6></a></div>
+              </div>
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/pizza.svg" width="60"
+                      alt="Pizza &amp; Pasta">
+                    <h6 class="fs-base text-center">Pizza &amp;
+                      Pasta</h6></a></div>
+              </div>
+            </div>
+            <div class="d-flex">
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/steak.svg" width="60"
+                      alt="Grill &amp; Steaks">
+                    <h6 class="fs-base text-center">Grill &amp;
+                      Steaks</h6></a></div>
+              </div>
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/fish.svg" width="60"
+                      alt="Fish &amp; Seafood">
+                    <h6 class="fs-base text-center">Fish &amp;
+                      Seafood</h6></a></div>
+              </div>
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/healthy.svg" width="60"
+                      alt="Healthy Food">
+                    <h6 class="fs-base text-center">Healthy Food</h6></a></div>
+              </div>
+            </div>
+            <div class="d-flex">
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/cuisine.svg" width="60"
+                      alt="Haute Cuisine">
+                    <h6 class="fs-base text-center">Haute Cuisine</h6></a></div>
+              </div>
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/chicken.svg" width="60"
+                      alt="Chicken &amp; Snaks">
+                    <h6 class="fs-base text-center">Chicken &amp;
+                      Snaks</h6></a></div>
+              </div>
+              <div class="mega-dropdown-column pt-4 px-3" style="width: 12rem;">
+                <div class="widget mb-3"><a class="d-block"
+                    href="food-delivery-category.html"><img
+                      class="d-block mx-auto mb-3"
+                      src="img/food-delivery/icons/coffee.svg" width="60"
+                      alt="Coffee &amp; Desserts">
+                    <h6 class="fs-base text-center">Coffee &amp;
+                      Desserts</h6></a></div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item"><a class="nav-link" href="index.html">All Food Market</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- Search collapse-->
+  <div class="search-box collapse" id="searchBox">
+    <div class="container py-2">
+      <div class="input-group"><i
+          class="ci-search position-absolute top-50 start-0 translate-middle-y ms-3"></i>
+        <input class="form-control rounded-start" type="text"
+          placeholder="What do you need?">
+      </div>
+    </div>
+  </div>
+</header>
